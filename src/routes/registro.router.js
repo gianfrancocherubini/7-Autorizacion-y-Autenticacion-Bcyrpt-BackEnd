ef9a68bb40afd52ed390e0cc120bb67aa8c6ebf6
gianfranco cherubini <cherussa@gmail.com>
2023-12-28 20:12:22 -0300

    listo bcrypt

--- src/routes/registro.router.js
+++ src/routes/registro.router.js
@@ -28,30 +28,18 @@ router.post('/', async (req, res) => {
     }
 
     if (email === 'adminCoder@coder.com' && password === 'coder123') {
-        // No incluir la contraseña en el objeto que se está creando
         try {
             let usuario = await UsuariosModelo.create({ nombre, email, rol: 'administrador' });
-            res.setHeader('Content-Type', 'application/json');
-            res.status(201).json({ success: true, message: 'Usuario administrador creado correctamente', usuario });
             res.redirect(`/api/login?message=Usuario ${email} registrado correctamente`);
         } catch (error) {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(500).json({ error: 'Error inesperado' });
             res.redirect('/api/registro?error=Error inesperado. Reintente en unos minutos');
         }
     } else {
-        // Si no es un usuario administrador, asigna el rol 'usuario'
-        // Utilizando la función creaHash de utils.js
         password = creaHash(password);
-
         try {
             let usuario = await UsuariosModelo.create({ nombre, email, password, rol: 'usuario' });
-            res.setHeader('Content-Type', 'application/json');
-            res.status(201).json({ success: true, message: 'Usuario creado correctamente', usuario });
             res.redirect(`/api/login?message=Usuario ${email} registrado correctamente`);
         } catch (error) {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(500).json({ error: 'Error inesperado' });
             res.redirect('/api/registro?error=Error inesperado. Reintente en unos minutos');
         }
     }
